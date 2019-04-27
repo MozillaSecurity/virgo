c16b6a2dc5b5e069054c288206a7943ff0ba38e8
posidron <1614333+posidron@users.noreply.github.com>
2019-04-27 10:00:38 +0200

    Update prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,20 @@
+/** @format */
+
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2, // Default
+  tabs: false, // Default
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed', // Default
+  jsxSingleQuote: false, // Default
+  trailingComma: 'none', // Default
+  bracketSpacing: true, // Default
+  jsxBracketSameLine: false, // Default
+  arrowParens: 'avoid', // Default
+  requirePragma: false, // Default
+  insertPragma: true,
+  proseWrap: 'preserve', // Default
+  htmlWhitespaceSensitivity: 'css', // Default
+  endOfLine: 'auto'
+}
