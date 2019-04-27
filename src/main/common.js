e47124a6ab9a5a8d6f770142be4f6718b3b3b835
posidron <1614333+posidron@users.noreply.github.com>
2019-04-27 10:35:29 +0200

    Add Environment class

--- src/main/common.js
+++ src/main/common.js
@@ -1,10 +1,21 @@
 /** @format */
+import electron from 'electron'
 export const appDefinition = require('../../package.json')
 
-export const isDevEnvironment = () => {
-  if ('ELECTRON_IS_DEV' in process.env) {
-    return parseInt(process.env.ELECTRON_IS_DEV, 10) === 1
-  } else {
-    return process.defaultApp || /node_modules[\\/]electron[\\/]/.test(process.execPath)
+export class Environment {
+  static isPackaged = () => {
+    return (electron.app || electron.remote.app).isPackaged
+  }
+
+  static isTest = () => {
+    return process.env.NODE_ENV === 'test'
+  }
+
+  static isProduction = () => {
+    return process.env.NODE_ENV === 'production'
+  }
+
+  static isDevelopment = () => {
+    return process.env.NODE_ENV === 'development'
   }
 }
