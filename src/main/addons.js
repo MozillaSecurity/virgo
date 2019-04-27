bfe8ec506e66ded050ce7d980515ad38a6644f20
posidron <1614333+posidron@users.noreply.github.com>
2019-04-27 10:33:44 +0200

    [Addons] minor improvements

--- src/main/addons.js
+++ src/main/addons.js
@@ -1,4 +1,5 @@
 /** @format */
+
 import logger from 'electron-log'
 import installExtension, {
   REACT_DEVELOPER_TOOLS,
@@ -9,17 +10,17 @@ import installExtension, {
 } from 'electron-devtools-installer'
 
 const toolMap = {
-  REACT_DEVELOPER_TOOLS: REACT_DEVELOPER_TOOLS,
-  REDUX_DEVTOOLS: REDUX_DEVTOOLS,
-  REACT_PERF: REACT_PERF,
-  MOBX_DEVTOOLS: MOBX_DEVTOOLS,
-  APOLLO_DEVELOPER_TOOLS: APOLLO_DEVELOPER_TOOLS
+  REACT_DEVELOPER_TOOLS,
+  REDUX_DEVTOOLS,
+  REACT_PERF,
+  MOBX_DEVTOOLS,
+  APOLLO_DEVELOPER_TOOLS
 }
 
 export const installDeveloperTools = tools => {
   tools.map(async toolName => {
     try {
-      let name = await installExtension(toolMap[toolName])
+      const name = await installExtension(toolMap[toolName])
       logger.info(`Added extension: ${name}`)
     } catch (error) {
       logger.info(`An error occurred: ${error}`)
