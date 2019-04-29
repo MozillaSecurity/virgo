ac56fc37c087b5eaf1ce14aec59ba72e25cae20c
posidron <1614333+posidron@users.noreply.github.com>
2019-04-29 05:01:07 +0200

    Improve stylelint and remove stylelint-processor-styled-components because of imcompatibility with plain CSS

--- configs/stylelint/stylelint.config.js
+++ configs/stylelint/stylelint.config.js
@@ -1,6 +1,5 @@
 /** @format */
 
 module.exports = {
-  processors: ['stylelint-processor-styled-components'],
-  extends: ['stylelint-config-recommended', 'stylelint-config-styled-components']
+  extends: ['stylelint-config-recommended', 'stylelint-prettier/recommended']
 }
