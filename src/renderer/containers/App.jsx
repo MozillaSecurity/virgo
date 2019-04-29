7e3153d7032a54c731f3cb40438ec63cd7cf849c
posidron <1614333+posidron@users.noreply.github.com>
2019-04-29 07:52:26 +0200

    Move App related code from index to main App container

--- src/renderer/containers/App.jsx
+++ src/renderer/containers/App.jsx
@@ -2,16 +2,33 @@
 
 import React from 'react'
 import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+/* Styles */
 import Reset from '@material-ui/core/CssBaseline'
-
+import { withStyles } from '@material-ui/core/styles'
+import '../styles/fontface-roboto.css'
+/* Custom UI */
 import SideDrawer from './Layout/SideDrawer'
 import SideDrawerList from './Layout/SideDrawerList'
 import ThemeProvider from './Themes/ThemeProvider'
-import ContentRoutes from '../ContentRoutes'
 
-export default class App extends React.Component {
-  render() {
-    return (
+import ContentRoutes from '../ContentRoutes'
+import { initStore } from '../store'
+
+// eslint-disable-next-line no-unused-vars
+const styles = theme => ({
+  '@global': {
+    '*, *::after, *::before': {
+      userSelect: 'none',
+      userDrag: 'none',
+      cursor: 'default !important'
+    }
+  }
+})
+
+const App = () => {
+  return (
+    <Provider store={initStore()}>
       <ThemeProvider>
         <Reset />
         <HashRouter>
@@ -20,6 +37,8 @@ export default class App extends React.Component {
           </SideDrawer>
         </HashRouter>
       </ThemeProvider>
-    )
-  }
+    </Provider>
+  )
 }
+
+export default withStyles(styles)(App)
--- src/renderer/index.jsx
+++ src/renderer/index.jsx
@@ -2,29 +2,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-
-import { withStyles } from '@material-ui/core/styles'
-import './styles/fontface-roboto.css'
 
 import App from './containers/App'
-import { initStore } from './store'
 
-const styles = theme => ({
-  '@global': {
-    '*, *::after, *::before': {
-      userSelect: 'none',
-      userDrag: 'none',
-      cursor: 'default !important'
-    }
-  }
-})
-
-const AppWithStyles = withStyles(styles)(App)
-
-ReactDOM.render(
-  <Provider store={initStore()}>
-    <AppWithStyles />
-  </Provider>,
-  document.getElementById('parcel-root')
-)
+ReactDOM.render(<App />, document.getElementById('parcel-root'))
