89e24178986d8721589bc592c305dfbd4680c32e
posidron <1614333+posidron@users.noreply.github.com>
2019-05-20 09:14:07 +0200

    Add forceDownload for Addons

--- src/main/addons.js
+++ src/main/addons.js
@@ -1,26 +1,20 @@
 /** @format */
 
 import logger from 'electron-log'
-import installExtension, {
-  REACT_DEVELOPER_TOOLS,
-  REDUX_DEVTOOLS,
-  REACT_PERF,
-  MOBX_DEVTOOLS,
-  APOLLO_DEVELOPER_TOOLS
-} from 'electron-devtools-installer'
+import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS, REACT_PERF } from 'electron-devtools-installer'
 
 const toolMap = {
   REACT_DEVELOPER_TOOLS,
   REDUX_DEVTOOLS,
-  REACT_PERF,
-  MOBX_DEVTOOLS,
-  APOLLO_DEVELOPER_TOOLS
+  REACT_PERF
 }
 
+// eslint-disable-next-line import/prefer-default-export
 export const installDeveloperTools = tools => {
+  const forceDownload = !!process.env.UPGRADE_EXTENSIONS
   tools.map(async toolName => {
     try {
-      const name = await installExtension(toolMap[toolName])
+      const name = await installExtension(toolMap[toolName], forceDownload)
       logger.info(`Added extension: ${name}`)
     } catch (error) {
       logger.info(`An error occurred: ${error}`)
