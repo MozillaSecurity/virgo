9009bfdf5ea94737a97b881848784a60992ad4ca
posidron <1614333+posidron@users.noreply.github.com>
2019-05-28 16:30:08 +0200

    Improve pref form handling and validation process #8

--- src/renderer/containers/Preferences/Docker.jsx
+++ src/renderer/containers/Preferences/Docker.jsx
@@ -1,5 +1,6 @@
 /** @format */
-import React, { useState } from 'react'
+
+import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
@@ -7,73 +8,105 @@ import PropTypes from 'prop-types'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core'
 
-import axios from 'axios'
 import * as actionCreators from '../../store/actions'
+import { URLValidator, EmailValidator } from '../../lib/validators'
 
 // eslint-disable-next-line no-unused-vars
 const styles = theme => ({})
 
-const DockerPrefs = props => {
-  const [value, setValue] = useState({
-    error: false,
-    status: 'The server which serves the JSON encoded task definitions.'
-  })
+const MyTextField = ({ ...props }) => {
+  return (
+    <TextField
+      {...props}
+      fullWidth
+      InputLabelProps={{
+        shrink: true
+      }}
+    />
+  )
+}
 
-  const updateTaskURL = URL => {
-    axios
-      .get(URL)
-      .then(() => props.updateTaskURL(URL))
-      .catch(error => setValue({ error: true, status: error.message }))
+class DockerPrefs extends React.Component {
+  state = {
+    taskURL: {
+      id: 'taskURL',
+      value: '',
+      label: 'Task Definition Server',
+      placeholder: this.props.taskURL,
+      error: false,
+      helperText: 'The server which serves the JSON encoded task definitions.',
+      isInvalid: value =>
+        value === '' || URLValidator.test(value) ? false : 'This appears not to be a valid URL.'
+    },
+    contactEmail: {
+      id: 'contactEmail',
+      value: '',
+      label: 'Contact Email',
+      placeholder: this.props.contactEmail,
+      error: false,
+      helperText: 'The contact address over which we shall notify you.',
+      isInvalid: value =>
+        value === '' || EmailValidator.test(value)
+          ? false
+          : 'This appears not to a valid Email address.'
+    }
   }
 
-  const updateContactEmail = email => {}
+  onChange = ({ target: { id, value } }) => {
+    const invalid = this.state[id].isInvalid(value)
 
-  return (
-    <form>
-      <List>
-        <ListItem>
-          <TextField
-            onKeyPress={event => {
-              if (event.key === 'Enter') {
-                updateTaskURL(event.target.value)
-                event.preventDefault()
-              }
-            }}
-            error={value.error}
-            type="url"
-            label="Task Definition Server"
-            placeholder={props.taskURL}
-            fullWidth
-            helperText={value.status}
-            InputLabelProps={{
-              shrink: true
-            }}
-          />
-        </ListItem>
-        <ListItem>
-          <TextField
-            onKeyPress={event => {
-              if (event.key === 'Enter') {
-                updateContactEmail(event.target.value)
-                event.preventDefault()
-              }
-            }}
-            error={value.error}
-            type="url"
-            label="Contact Email"
-            placeholder={props.contactEmail}
-            fullWidth
-            helperText={value.status}
-            InputLabelProps={{
-              shrink: true
-            }}
-          />
-        </ListItem>
-      </List>
-    </form>
-  )
+    this.setState({
+      ...this.state,
+      [id]: {
+        ...this.state[id],
+        value,
+        error: Boolean(invalid),
+        helperText: invalid || this.state[id].helperText
+      }
+    })
+  }
+
+  apply = event => {
+    event.preventDefault()
+
+    /* Check if there are any errors in our state. */
+    const errors = Object.keys(this.state)
+      .map(group => this.state[group])
+      .filter(item => item.error === true)
+
+    if (errors.length !== 0) {
+      return
+    }
+
+    this.props.updateTaskURL(this.state.taskURL.value || this.state.taskURL.placeholder)
+    this.props.updateContactEmail(this.state.contactEmail.value || this.state.taskURL.placeholder)
+
+    console.log(`Applied${JSON.stringify(this.state)}`)
+  }
+
+  render() {
+    const { taskURL, contactEmail } = this.state
+
+    return (
+      <form noValidate>
+        <List>
+          <ListItem>
+            <MyTextField {...taskURL} onChange={this.onChange} />
+          </ListItem>
+          <ListItem>
+            <MyTextField {...contactEmail} onChange={this.onChange} />
+          </ListItem>
+          <ListItem />
+        </List>
+        <Button type="button" onClick={this.apply} label="Save" color="primary" variant="contained">
+          Apply
+        </Button>
+      </form>
+    )
+  }
 }
 
 DockerPrefs.propTypes = {
@@ -94,7 +127,7 @@ const mapDispatchToProps = dispatch => {
   return bindActionCreators(
     {
       updateTaskURL: actionCreators.updateTaskURL,
-      updateContactEmail: actionCreators.contactEmail
+      updateContactEmail: actionCreators.updateContactEmail
     },
     dispatch
   )
