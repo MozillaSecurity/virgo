59e36858dac2b5e0429722d2bad7fcbb26ace5ed
posidron <1614333+posidron@users.noreply.github.com>
2019-06-18 11:06:14 -0700

    Add isReachable and isOnline

---
+++ src/renderer/lib/monitor.js
@@ -0,0 +1,24 @@
+/** @format */
+
+import http from 'http'
+import https from 'https'
+import { promises as DNS } from 'dns'
+
+export const isOnline = (host = 'mozilla.org') => {
+  return DNS.lookup(host)
+}
+
+export const isReachable = (url, allowStatus = [302]) => {
+  return new Promise((resolve, reject) => {
+    return https.get(url, response => {
+      if (response.statusCode >= 200 && response.statusCode <= 300) {
+        resolve({ code: response.statusCode })
+      }
+      if (allowStatus.includes(parseInt(response.statusCode, 10))) {
+        resolve({ code: response.statusCode })
+      }
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject({ code: response.statusCode, message: http.STATUS_CODES[response.statusCode] })
+    })
+  })
+}
